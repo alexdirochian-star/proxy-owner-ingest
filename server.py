6bb5420848d403ef91cbefc00b282d84b699dc61
alexdirochian-star <alexdirochian@gmail.com>
2026-02-07 08:06:20 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
     def do_POST(self):
         length = int(self.headers.get('Content-Length', 0))
         body = self.rfile.read(length)
@@ -21,5 +31,3 @@ class Handler(BaseHTTPRequestHandler):
 server = HTTPServer(("0.0.0.0", 10000), Handler)
 print("Server running...")
 server.serve_forever()
-
-
