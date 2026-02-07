3a6cad62b2c158f758dbf9ba1f8507d59565a37c
alexdirochian-star <alexdirochian@gmail.com>
2026-02-07 10:22:29 -0500

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,11 @@ class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers.get('Content-Length', 0))
         body = self.rfile.read(length)
+from urllib.parse import parse_qs
+
+params = parse_qs(body.decode())
+call_status = params.get("CallStatus", [""])[0]
+print("CALL STATUS:", call_status)
 
         print("RECEIVED POST FROM TWILIO:")
         try:
